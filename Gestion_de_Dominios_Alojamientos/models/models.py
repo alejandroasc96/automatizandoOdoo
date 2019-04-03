27e4cd09589b116b8600765a0d6708ffd69e097c
alejandroasc <alejandroasc96gmail.com>
2019-04-03 08:18:45 +0100

    Charla Tiburcio 3-4-19

--- Gestion_de_Dominios_Alojamientos/models/models.py
+++ Gestion_de_Dominios_Alojamientos/models/models.py
@@ -25,7 +25,7 @@ class campo_calculado(models.Model):
     campoCalculado = fields.Integer(string='Días que han pasado',compute="_campocalculado",store=False)
     # referenciamos el campo fechaAle, que se encuentra en el modelo product.temple a account.invoice.line con el nombre que aparece a la izquierda
     
-    rel_field = fields.Integer(string='Fecha Caducidad Establcida',related='product_id.subscriptionDays')
+    rel_field = fields.Integer(string='Fecha Caducidad Establecida',related='product_id.subscriptionDays')
     total = fields.Integer(string='Días restantes ',compute='_total',store=False)
 
     @api.one
@@ -50,20 +50,61 @@ class caducidad_productos(models.Model):
         print ("### Revisando las Facturas Vencidas")
         for r in total_dias:
             ale = r.rel_field - ((datetime.now() - datetime.strptime(r.create_date, '%Y-%m-%d %H:%M:%S')).days)
-            if (ale=30):
+            if (ale<=30):
                 mail_from = r.invoice_id.user_id.email
                 mail_to = r.invoice_id.partner_id.email
                 userID = r.invoice_id.user_id.id
 
                 mail_vals = {
                             'subject': 'Notificacion de Facturas Vencidas',
-                            'author_id': userID, #my_user.id,
+                            'author_id': userID,
                             'email_from': mail_from,
                             'email_to': mail_to,
                             'message_type':'email',
                             'body_html': 'Se te están caducadon las suscripciones',
                                 }
-                # template_obj = self.env['email.template'].sudo().search([('name','=','Create Section for Thesis')], limit=1)# Código Para Tiburcio
-                mail_id = self.env['mail.mail'].create(mail_vals)
-                mail_id.send()
-                _logger.warning("----------------------------" + str(r.invoice_id))
+                template_obj = self.env['mail.template'].search([('name','=','Tiburcio')], limit=1) # Código Para Tiburcio
+            #     email_ids.append(self.pool.get('mail.mail').create(cr, uid, vals, context=context))
+            # if email_ids:
+            #     self.pool.get('mail.mail').send(cr, uid, email_ids, context=context)
+                # mail_id = self.env['mail.mail'].create(mail_vals)
+                # mail_id.send()
+                template_obj.generate_mail(r.id)
+                mail_id = self.env['mail.mail'].create(template_obj)
+                # _logger.warning("----------------------------" + str(r.invoice_id))
+
+        # def send_email(self,cr,uid,ids,context=None):
+        #     template_obj = self.pool.get('mail.template')
+        #     ir_model_data = self.pool.get('ir.model.data')
+        #     # Create a template id by either of the ways.
+        #     #template_id = template_obj.create(cr, uid,{'name':'Template Name','model_id':'Your model id'})
+        #     template_id= ir_model_data.get_object_reference(cr, uid, 'module_name', 'xml_template_id')[1]
+        #     if template_id:
+        #         #------------ if a template id is created -------------------
+        #         values = template_obj.generate_email(cr, uid, template_id, ids[0], context=context)
+        #         #  Set/Modify the values for the template.
+        #         values['subject'] = subject you want to show
+        #         values['email_to'] = receiver of the email
+        #         values['partner_to'] = partner ids
+        #         values['body'] = body_html
+        #             .....
+        #             .....
+        #         #--------------------------------------------------------------
+        #         #----------------if template id is not created-----------------
+        #         values = {
+        #         'subject': 'subect ',
+        #         'body_html': 'Message to be sent',
+        #         'email_to': receiver email,
+        #         'email_from': 'sender_email',
+        #         values['partner_to'] : partner ids
+        #         ......
+        #         ......
+        #         }
+        #     #---------------------------------------------------------------
+        #     mail_obj = self.pool.get('mail.mail') 
+        #     msg_id = mail_obj.create(cr, uid, values, context=context) 
+        #     if msg_id: 
+        #         mail_obj.send(cr, uid, [msg_id], context=context) 
+        #     return True
+    
+    
