7055241d6e9a3c370e9c2502bb5e0a380d7e2f04
AllWKA <bryanjaramillobaldeon@gmail.com>
2019-04-29 13:58:23 +0100

    fine version

--- Gestion_de_Dominios_Alojamientos/models/models.py
+++ Gestion_de_Dominios_Alojamientos/models/models.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 
 class DeadLineTime(models.Model):
     _name = 'deadline'
-    months = fields.Integer( string="months of services", store=True)
+    months = fields.Integer(string="months of services")
 
 class serviceInvoiceLine(models.Model):
     _name = 'account.invoice.line'     
@@ -28,91 +28,17 @@ class serviceInvoiceLine(models.Model):
     def _getDateToDie(self):
         for r in self:
             r.date_to_end = datetime.strptime(
-                r.create_date, '%Y-%m-%d %H:%M:%S') + timedelta(r.product_id.dead_line_service.months * 30)
+                r.create_date, '%Y-%m-%d %H:%M:%S') + timedelta((r.product_id.dead_line_service.months/12 * 365)+((r.product_id.dead_line_service.months%12) * 30))
             
 
 class Service(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
 
-    dead_line_service = fields.Many2one('deadline', string="Caduca en :", store=True)
+    dead_line_service = fields.Many2one('deadline', string="Dead Line Service")
     
     
 
-class revisando_factura_clientes(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
     
-    @api.model
-    def revisando_factura_clientes(self):
-        clientes = self.search([])
-        for r in clientes:
-            facturas = r.invoice_ids
-            arrayDeDict = []
-            diccionario = {}
-            fecha = datetime.strftime(datetime.now(),"%d/%m/%Y %H:%M:%S")
-            for t in facturas:
-                linea = t.invoice_line_ids
-                for j in linea:
-                    ale = j.rel_field - ((datetime.now() - datetime.strptime(j.create_date, '%Y-%m-%d %H:%M:%S')).days)
-                    if (ale<=30):
-                        diccionario = {1: j.product_id.name,
-                                        2: j.product_id,
-                                        3: j.quantity,
-                                        4: 0.00,}
-                        arrayDeDict.append(diccionario)
-                        
-            self.create_sales_order( arrayDeDict, r.id)
-            
-
-    @api.model
-    def create_sales_order(self, arrayDeDict, customer_id):
-        sale_pool = self.env['sale.order']
-        prod_pool = self.env['product.product']
-        sale_line_pool = self.env['sale.order.line']
-        sale_no = ''
-        sale = {}
-        if customer_id:
-            customer_id = int(customer_id)
-            sale = {'partner_id': customer_id, 
-                    'partner_invoice_id': customer_id,
-                    'partner_shipping_id': customer_id}
-            sale_id = sale_pool.create(sale)
-            if sale_id:
-                sale_brw = sale_id
-                sale_brw.onchange_partner_id()
-        for lineOrder in arrayDeDict:
-            #create sale order line
-            sale_line = {}
-            prod_rec = lineOrder.get(2)
-            cantidad = lineOrder.get(3)
-            if prod_rec:
-                sale_line = {'name': prod_rec.name or False,
-                                'product_id': prod_rec.id,
-                                'product_uom_qty': cantidad,
-                                'discount':  lineOrder.get(4),
-                                'order_id': sale_id.id}
-                sale_line_id = sale_line_pool.create(sale_line)
-        send_sale_order = sale_id.force_quotation_send()
-        return {"name": sale_brw.name, "id": sale_brw.id } 
-    
-
-
-class review_quotation(models.Model):
-    _name = 'sale.order'
-    _inherit = 'sale.order'
-    
-    @api.model
-    def review_quotation(self):
-        #search quotations how has state = sent (sent quotation)
-        quotation = self.search([('state', 'in', ['sent'])]) 
-
-        for quo in quotation:
-            date = (datetime.now() - datetime.strptime(quo.date_order, '%Y-%m-%d %H:%M:%S')).days
-            _logger.warning("--SentQuotation--------------------------" + str(date))
-            _logger.warning("--SentQuotation--------------------------" + str(quo))
-            #enviar mensaje a user
-            ale = self.env['mail.thread']
-            ale.message_post(body='<p>hola</p>', partner_ids=[1, 6])
 
 
