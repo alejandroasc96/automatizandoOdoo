d8fe47794a4a4dfe7ab2bba0e6cc96738a8e1257
alejandroasc96 <alejandroasc96@gmail.com>
2019-05-23 13:30:37 +0100

    Entrega del módulo a David

--- gestion_servicios/models/models.py
+++ gestion_servicios/models/models.py
@@ -8,14 +8,15 @@ from dateutil.relativedelta import relativedelta
 import logging
 
 _logger = logging.getLogger(__name__)
-# _logger.warning("----------------------------" + str(r.invoice_id))
 
-class addNewFieldResPartner(models.Model):
+
+class addDepartmentFieldResPartner(models.Model):
     
     _inherit = 'res.partner'
 
     department = fields.Char(string='Departamento')
 
+# Fecha formateada para el PDF
 class dateOrderFormat(models.Model):
     _name = 'sale.order'
     _inherit = 'sale.order'
@@ -31,18 +32,19 @@ class dateOrderFormat(models.Model):
             line.date_order_format = b 
 
 
-
+# Duracion del servico en meses
 class DeadLineTime(models.Model):
     _name = 'deadline'
     _rec_name = 'months'
 
     months = fields.Integer(string="months of services")
 
+
 class stateListener(models.Model):
     _name = 'account.invoice'     
     _inherit = 'account.invoice'
     
-
+# code to calculate when the service from inoice.line will end
     @api.model
     @api.multi
     def invoice_validate(self):
@@ -51,28 +53,26 @@ class stateListener(models.Model):
             invoice.message_subscribe([invoice.partner_id.id])
         self._check_duplicate_supplier_reference()
 
-        # code to calculate when the service from inoice.line will end
+        
         for line in self.invoice_line_ids:
             if line.product_id.type == 'service':
                 #Para calcular la fecha en la que caduca el servicio usamos el método relativedelta que nos va añadiendo
                 #el número de meses que están estipulados.
                 line.date_to_end = datetime.now() + relativedelta(months=line.product_id.dead_line_service.months)
-                # line.only_date = datetime.strptime(line.date_to_end, '%Y-%m-%d')
-                # _logger.warning("----------------------------" + str(line.only_date))
                 pass
             pass
 
         return self.write({'state': 'open'})
 
+# Clase que añada dos nuevos campos a account.invoice.line
 class serviceInvoiceLine(models.Model):
     _name = 'account.invoice.line'     
     _inherit = 'account.invoice.line'
 
     date_to_end = fields.Date(string='Date To Die',store=True)
-    # only_date = fields.Date(string='Date',store=True)
     days_to_end = fields.Integer(string='Days To Die', compute="_getDaysToDie", store=False)
     
-
+    # Se calculan dichos campos según condiciones
     @api.depends('create_date')
     def _getDaysToDie(self):
         for line in self:
@@ -85,6 +85,7 @@ class serviceInvoiceLine(models.Model):
             pass
             
 
+# Creacion de relacion entre producto y durancion del servicio
 class Service(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
@@ -93,10 +94,12 @@ class Service(models.Model):
     
     
 
+
 class revisando_factura_clientes(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
     
+    # Revisando las caducidad de todos los servicios contratados por todos los clientes
     @api.model
     def revisando_factura_clientes(self):
         clientes = self.search([])
@@ -111,7 +114,8 @@ class revisando_factura_clientes(models.Model):
                         for j in linea:
                             if j.product_id.type == 'service':
                                 expirationDate = j.days_to_end
-                                if expirationDate <= 31:
+                                # Falta comprobar que la línea de producto no haya sido enviada
+                                if expirationDate <= 31 and expirationDate > 15:
                                     diccionario = {1: j.product_id.name,
                                                     2: j.product_id,
                                                     3: j.quantity,
@@ -120,6 +124,7 @@ class revisando_factura_clientes(models.Model):
                                     
                 self.create_sales_order( arrayDeDict, r.id)
 
+    # Creando el presupuesto a notificar al cliente por correo
     @api.model
     def create_sales_order(self, linesOrder, customer_id):
         sale_pool = self.env['sale.order']
@@ -148,8 +153,9 @@ class revisando_factura_clientes(models.Model):
                             #  le dices quien es el padre
                             'order_id': sale_id.id}
                 sale_line_id = sale_line_pool.create(sale_line)
-        # añadiendo el attachment
+        # Realizando el envio del presupuesto al cliente
         email_act = sale_id.action_quotation_send()
+        # añadiendo el attachment
         if email_act and email_act.get('context'):
             email_ctx = email_act['context']
             email_ctx.update(default_email_from=sale_id.partner_id.email)
@@ -167,19 +173,18 @@ class review_quotation(models.Model):
         quotation = self.search([('state', 'in', ['sent'])])
         for quo in quotation:
             date = (datetime.now() - datetime.strptime(quo.date_order, '%Y-%m-%d %H:%M:%S')).days
-            _logger.warning("----------------------------" + str(date))
-            if date <=30:
+            # hay que comprobar que no haya sido notificado ya
+            if date =15 and quo.state == 'sent':
                 
                 self._postDeniedOrder({"quotation_id" : quo.id, "partner_id": quo.partner_id.name})
 
-#enviar mensaje a Channel
+#enviar notificacion a Channel del presupuesto sin respuesta
     @api.model
     def _postDeniedOrder(self, order):
         
         channels = self.env['mail.channel'].search([])
         for channel in channels:
             if channel.name == "Denied Orders":
-                _logger.warning("----------------------------" + str(order.get("quotation_id")))
                 body = "Factura : " + str(order.get("quotation_id")) + " Cliente :" + str(order.get("partner_id"))
                 channel.message_post(subject="order denied", body= body , subtype="mail.mt_comment")
                 
